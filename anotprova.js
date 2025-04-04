d9be725b7e1e7d8fb6a5747a6ac625bb63dccbad
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-04 16:34:39 -0300

    Create anotprova.js

---
+++ anotprova.js
@@ -0,0 +1,19 @@
+const carro = {
+    marca: 'chevrolet',
+    modelo: 'celta',
+    ano: '1994'
+};
+console.log(carro.marca); 
+console.log(carro.modelo); 
+console.log(carro.ano); 
+//--------------------------------
+
+const usuario = {
+    nome: "Fernanda",
+    email: "fernanda@email.com",
+    idade: 25
+};
+delete usuario.email;
+console.log(usuario); 
+//-------------------------------
+
