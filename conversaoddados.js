ed302deeebc6fbe69e28698c4d9a830234e10c2a
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-11 11:53:25 -0300

    Create conversaoddados.js

---
+++ conversaoddados.js
@@ -0,0 +1,23 @@
+//conversão de tipos
+console.log("ano"+ 2025)
+console.log("3"+"5")
+
+//conversão explícita
+console.log(parseInt("3")+ parseInt("5")) //parseint deixa string para int, parsefloat deixa em float
+
+//conversão implícita
+console.log("10"/"2")  //transforma string em int automaticamente
+
+let cliente = "Maria"
+let maiorDeIdade = true
+let saldo = 100
+
+console.log(typeof cliente) //identifica tipos
+console.log(typeof saldo) 
+console.log(typeof maiorDeIdade) 
+
+cliente = "Rafael"
+console.log(cliente)
+
+const nome = "Luis" //constante, não pode ter seu valor alterado
+nome = "João"
