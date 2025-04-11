87bb6e6e53fe8253c7c17707861270da80bbe81e
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-11 11:52:34 -0300

    Create condicao.js

---
+++ condicao.js
@@ -0,0 +1,12 @@
+const idade = 18
+const carteirahabilitacao = true // === : compara os tipos se for int precisa ser int
+                                 // == : não compara os tipos, mesmo de for int pode ser string
+if(idade >=18 && carteirahabilitacao){ // && = e, || = ou no Python
+    console.log("Pode dirigir")
+}
+
+//if (idade === 18){ //=== : precisa ser exatamente 18
+    //console.log("Maior de Idade")
+//}else{
+    //console.log("Menor de Idade")
+//} 
