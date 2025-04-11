c0c24ad49bf63169617da85673f320bbd75fecd1
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-11 11:55:44 -0300

    Create operadAritmet.js

---
+++ operadAritmet.js
@@ -0,0 +1,10 @@
+let soma = 10 + 20
+let sub = 20 - 10
+let divisao = 20/2
+let mult = 10*2
+let poten = 10**2
+console.log("Soma: ", soma)
+console.log("Subtração: ",sub)
+console.log("Divisão: ", divisao)
+console.log("Multiplicação: ", mult)
+console.log("Potênciação: ", poten)
