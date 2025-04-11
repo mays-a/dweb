a7403f628f3a1b14f5cc6ec4616f4a5777b4a958
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-11 11:59:27 -0300

    Update conversaoddados.js

--- conversaoddados.js
+++ conversaoddados.js
@@ -1,3 +1,10 @@
+//tipos de dados
+let nome = 'Pedro' // variável
+let numero = 20
+let bool = false 
+
+console.log(numero)  // console.log = print
+
 //conversão de tipos
 console.log("ano"+ 2025)
 console.log("3"+"5")
