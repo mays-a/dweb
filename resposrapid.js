b19515f8880e3634cab7948f0a99758bac8836a4
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-04-11 15:17:38 -0300

    Create resposrapid.js

---
+++ resposrapid.js
@@ -0,0 +1,15 @@
+//12. Qual a diferença entre var, let e const?
+//var tem escopo global ou de função
+//let tem escopo de bloco
+//const cria uma variável constante que não pode ser alterada após a sua inicialização.
+
+//13. O que acontece quando você tenta acessar uma variável que ainda não foi definida?
+//acontece erro no resultado, como NameError ou ReferenceError
+
+//14. Como funciona o typeof? Escreva um código usando o typeof
+//um operador unário que retorna uma string indicando o tipo de um operando
+
+
+
+
+
