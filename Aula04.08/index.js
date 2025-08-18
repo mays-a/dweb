0560dc24f469fc77e7d4ea40a84a2f0c615992d5
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-08-18 17:38:37 -0300

    Update index.js

--- Aula04.08/index.js
+++ Aula04.08/index.js
@@ -1,7 +1,7 @@
 // Importa a função criarItemDaLista do arquivo especificado
 import { criarItemDaLista } from "./scripts/CriarItemDaLista.js";
 // Importa a função verificarListaVazia do arquivo especificado
-//import verificarListaVazia from "./scripts/VerificarListaVazia.js";
+import verificarListaVazia from "./scripts/VerificarListaVazia.js";
 
 // Seleciona o input onde o usuário digita o item
 const inputItem = document.getElementById("input-item");
@@ -9,6 +9,7 @@ const inputItem = document.getElementById("input-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
 // Seleciona o botão de adicionar item
 const botaoAdicionar = document.getElementById("adicionar-item");
+botaoAdicionar.style.cursor = 'pointer';
 
 // Adiciona um ouvinte de evento de clique ao botão de adicionar
 botaoAdicionar.addEventListener("click", (evento) => {
@@ -19,11 +20,12 @@ botaoAdicionar.addEventListener("click", (evento) => {
     // Adiciona o novo item à lista de compras
     listaDeCompras.appendChild(itemDaLista);
     // Verifica se a lista está vazia e atualiza a interface se necessário
-    //verificarListaVazia(listaDeCompras);
+    verificarListaVazia(listaDeCompras);
     // Limpa o campo de input após adicionar o item
     inputItem.value = "";
 });
 
 // Faz uma verificação inicial se a lista está vazia ao carregar a página
-//verificarListaVazia(listaDeCompras);
+verificarListaVazia(listaDeCompras);
+
 
