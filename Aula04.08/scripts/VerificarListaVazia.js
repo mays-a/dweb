a50bcf78a0cdc33847b60ccf27a39ce1e6e2464e
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-08-18 17:37:20 -0300

    Update VerificarListaVazia.js

---
+++ Aula04.08/scripts/VerificarListaVazia.js
@@ -0,0 +1,17 @@
+const mensagemListaVazia = document.querySelectorAll('mensagem-lista-vazia');
+
+function verificarListaVazia(listaDeCompras){
+    //Seleciona todos os elementos <li> da lista de compras e retorna um array
+    const itensDaLista = listaDeCompras.querySelectorAll('li');
+    //Se nn houver nenhum <li> na lista (lista vazia)
+    if (itensDaLista.length === 0){
+        //exibe mensagem de lista vazia
+        mensagemListaVazia.style.display = 'black';
+    }else {
+        // 'none' Esconde a mensagem
+        mensagemListaVazia.style.display = 'none';
+    }
+}
+
+//Exporta a fuhnção para ser usada em outro arquivo js
+export default verificarListaVazia;
