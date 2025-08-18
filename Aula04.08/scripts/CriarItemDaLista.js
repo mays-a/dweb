6b3f7d8083cac0895a2f15713b642c4edb3582eb
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-08-18 17:47:05 -0300

    Update CriarItemDaLista.js

--- Aula04.08/scripts/CriarItemDaLista.js
+++ Aula04.08/scripts/CriarItemDaLista.js
@@ -1,4 +1,5 @@
 export const inputItem = document.getElementById('input-item');
+let contador = 0;
 export function criarItemDaLista(){
     if (inputItem.value === '') {
         alert("Por favor, insira um item!");
@@ -9,9 +10,16 @@ export function criarItemDaLista(){
     containerItemDaLista.classList.add("lista-item-container")
     const nomeItem = document.createElement("p")
 
+    //Cria um checkbox para o item da lista
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox"; //define tipo do input como checkbox
+    inputCheckBox.id = "checkbox-" + contador++ //define id unico para checkbox
+    containerItemDaLista.appendChild(inputCheckBox);
+    
     nomeItem.innerText = inputItem.value;
     containerItemDaLista.appendChild(nomeItem);
     itemDaLista.appendChild(containerItemDaLista);
     return itemDaLista;
 }
+
 
