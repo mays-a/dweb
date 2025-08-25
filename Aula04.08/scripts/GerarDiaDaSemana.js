cdd7550a524042f06332f4af56fcf6732364b394
mays-a <o.maysa@aluno.ifsp.edu.br>
2025-08-25 17:54:57 -0300

    Update GerarDiaDaSemana.js

---
+++ Aula04.08/scripts/GerarDiaDaSemana.js
@@ -0,0 +1,18 @@
+function gerarDiaDaSemana(){
+    //Manipula data e hora
+    const diaDaSemana = new Date().toLocaleDateString('pt-br', {
+        weekday: long
+    });
+
+    const hora = new Date().toLocaleTimeString('pt-br', {
+        hour: 'numeric',
+        minute: "numeric"
+    })
+
+    const data = new Date().toLocaleDateString("pt-br");
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+
+    return dataCompleta;
+}
+
+export default gerarDiaDaSemana;
